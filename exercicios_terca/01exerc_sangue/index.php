84c4f83210228f03d72ea0fd899825baea859440
Brendon Uehara <100155792+brendonuehara@users.noreply.github.com>
2022-04-08 15:06:02 -0300

    Update index.php

--- exercicios_terca/01exerc_sangue/index.php
+++ exercicios_terca/01exerc_sangue/index.php
@@ -4,22 +4,75 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Centro de Doação de Sangue</title>
+    <title>Atividade Doação de Sangue</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Righteous&family=Sarala:wght@400;700&display=swap&family=Lato:wght@100&family=Roboto+Condensed:ital@1&display=swap&family=Lato:wght@100;400&display=swap');
+        * {
+            text-decoration: none;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            font-size: 100%;
+            background-color: black;
+        }
+
+        .cabecalho {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            padding: 24px;
+        }
+
+        .cabecalho-titulo {
+            font-size: 28px;
+            font-weight: 600;
+            font-family: 'Roboto Condensed', sans-serif;
+            color: #ffffff;
+        }
+
+        .conteudo {
+            border-top: 1px solid #FFF2E7
+        }
+
+        .conteudo-principal {           
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            flex-direction: column;
+            gap: 30px;
+            color: white;
+            padding: 48px;
+        }
+
+        .conteudo-principal-paragrafo {
+            font-family: 'Roboto', sans-serif;
+            font-weight: 400;
+            font-size: 14px;
+        }
+
+    </style>
 </head>
 <body>
+    <header class="cabecalho">
+    <h2 class="cabecalho-titulo">Informações do Doador</h2>
+    </header>
 
-    <?php 
-    $nome = 'Brendon';
-    $peso =  69; 
-    $idade = 70;
+    <?php
+        $nome = 'a';
+        $idade = 15;
+        $peso = 49;
     ?>
+    
+    <main class="conteudo">
+        <div class="conteudo-principal">
+        <p class="conteudo-principal-paragrafo"><b>Nome do Doador: </b> <?= $nome ?></p>
+        <p class="conteudo-principal-paragrafo"><b>Peso do Doador: </b><?= $peso ?> Kg</p>
+        <p class="conteudo-principal-paragrafo"><b>Idade do Doador: </b><?= $idade ?> anos</p>
 
-    <h2>Informações do Doador</h2>
-    <p><b>Nome do Doador: </b> <?= $nome ?></p>
-    <p><b>Peso do Doador: </b><?= $peso ?></p>
-    <p><b>Idade do Doador: </b><?= $idade ?></p>
-
-    <?php        
+        <?php       
         if($idade < 16 && $peso < 50) {
             echo "O doador não está apto pra doar sangue por conta da idade e do peso! Idade abaixo dos 16 anos e peso abaixo dos 50kg. ";
         }
@@ -39,6 +92,8 @@
             echo "O doador está apto pra doar sangue! ";
         }
         
-    ?>
+        ?>
+        </div>
+    </main>
 </body>
 </html>
